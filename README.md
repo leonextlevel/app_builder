3dee85b6e4f10beab325ddd86f77c4c8196f0bb7
LeoNextLevel <leandrolopessjc15@gmail.com>
2021-11-21 21:25:36 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# App Builder
+
+Aplicativo para controle do dia a dia de treino.
+
+## Pré-requisitos
+Instale o Quasar CLI na máquina.
+
+## Instalar as dependências
+```bash
+npm install
+```
+
+### Executar em modo dev
+```bash
+quasar dev -m pwa
+```
+
+### Executar o linter
+```bash
+npm run lint
+```
+
+### Build para produção
+```bash
+quasar build
+```
+
+### Cores do projeto
+- Vermelho: #8a1c3dff
+- Preto: #000000ff
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,99 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          App Builder
+        </q-toolbar-title>
+
+        <div>Alpha Version</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Menu
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+  {
+    title: 'Meu Treino',
+    caption: 'Exercícios',
+    icon: 'fitness_center',
+    link: '/'
+  },
+  {
+    title: 'Evolução',
+    caption: 'Histórico',
+    icon: 'trending_up',
+    link: '/'
+  },
+  {
+    title: 'Peso',
+    caption: 'Histórico',
+    icon: 'monitor_weight',
+    link: '/'
+  },
+  {
+    title: 'Tempo',
+    caption: 'Cronômetro, Temporizador',
+    icon: 'alarm',
+    link: '/'
+  }
+]
+
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
